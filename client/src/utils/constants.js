6981da0d498760e2e6b2541569616d0b44fa136c
KING-Samm <kcsamm11@gmail.com>
2022-04-07 23:50:23 +0100

    Copied Transactions.json into utils folder. Created constants.js in same folder
    Configured utilities to enable client interact with smart contract

---
+++ client/src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from './Transactions.json';
+
+
+// Export the contract ABI and address which were generated on deploy of smart contract (i.e Transactions.sol)
+export const contractABI = abi.abi;
+export const contractAddress = '0x14614a40D29D55B71D5A55b8CFE63b5592b113D0';
