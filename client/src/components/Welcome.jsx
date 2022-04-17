08635977986b3d9aaba7393b72574e334af04947
KING-Samm <kcsamm11@gmail.com>
2022-04-17 23:01:54 +0100

    Fixed bugs. Started work on services component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -19,7 +19,7 @@ const Input = ({ placeholder, type, name, value, handleChange }) => (
         onChange={ (e) => handleChange(e, name) }
         className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
     />
-)
+);
 
 // Welcome (Main) Component 
 const Welcome = () => {
