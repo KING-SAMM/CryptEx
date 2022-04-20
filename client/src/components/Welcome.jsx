b9c107d161404b7ad9b21a1bf4b93c834b68da44
KING-Samm <kcsamm11@gmail.com>
2022-04-20 22:57:11 +0100

    Included dummyData.js. Working on Transactions component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -109,7 +109,7 @@ const Welcome = () => {
                             </div>
                             <div>
                                 <p className="text-white font-light text-sm">
-                                    Address
+                                    {Address}
                                 </p>
                                 <p className="text-white font-semibold text-lg mt-1">
                                     Ethereum
