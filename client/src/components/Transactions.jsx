ced95182d2b81377a7a11965b02458dd5ededede
KING-Samm <kcsamm11@gmail.com>
2022-04-22 09:21:08 +0100

    Created useFetch hook. Completed dummy transactions gifs

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -4,10 +4,13 @@ import { TransactionContext } from '../context/TransactionContext';
 
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from "../utils/shortenAddress"; 
+import useFetch from "../hooks/useFetch";
 
 // Transactions card component 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => 
 {
+    const gifUrl = useFetch({ keyword });
+
     return (
         <div className="bg-[#18191A] m-4 flex flex-1
         2xl:min-w-[450px] 
@@ -37,6 +40,12 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
                             <p className="text-white text-base">Message: { message }</p>
                         </>
                     ) }
+
+                    <img 
+                        src={ gifUrl || url } 
+                        alt="gif" 
+                        className="w-full h-64 2x:h-96 rounded-md shadow-lg object-cover"
+                    />
 
                     {/* Timestamp  */}
                     <div className="bg-black p-3 px-5 w-max rounded-3xl mt-5 shadow-2xl">
