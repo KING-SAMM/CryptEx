997024a0bc87c873180a03c7b20cbd968056b69e
KING-Samm <kcsamm11@gmail.com>
2022-04-23 00:49:34 +0100

    Fixed bugs

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -55,6 +55,8 @@ export const TransactionProvider = ({ children }) => {
     // Store and keep track of transaction count
     const [ transactionCount, setTransactionCount ] = useState(localStorage.getItem('transactionCount'));
 
+    const [ transactions, setTransactions ] = useState([]);
+
     // Handle input value change
     const handleChange = (e, name) => 
     {
@@ -78,11 +80,16 @@ export const TransactionProvider = ({ children }) => {
 
             const structuredTransactions = availableTransactions.map((transaction) => ({
                 addressTo: transaction.receiver,
-                addressFrom: trabaction.sender,
-                timestamp: new Date(transaction.timestamp.toNumber * 1000)
+                addressFrom: transaction.sender,
+                timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+                message: transaction.message,
+                keyword: transaction.keyword,
+                amount: parseInt(transaction.amount._hex) / (10 ** 18)
             }));
 
-            console.log(availableTransactions);
+            console.log(structuredTransactions);
+            
+            setTransactions(structuredTransactions);
         } catch (error) {
             console.error(error);
         }
